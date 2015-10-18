2d2b5de85c875565939107a1beab3ddd3d064dbf
ilya-b <ilya-b@yandex-team.ru>
2015-10-18 12:06:31 +0300

    Replace setq with let

--- config.el
+++ config.el
@@ -69,83 +69,84 @@
 (defun core-grunt-tests ()
   "Invokes grunt test task and shows output"
   (interactive)
-  (core-try-start-process "grunt test"))
+  (core-try-start-process "grunt test" t))
 
 (defun core-npm-tests ()
   "Invokes grunt test task and shows output"
   (interactive)
-  (core-try-start-process "npm run test"))
+  (core-try-start-process "npm run test" t))
 
 (defun core-grunt-build ()
   "Invokes grunt buil and shows output"
   (interactive)
-  (core-try-start-process "grunt build"))
+  (core-try-start-process "grunt build" t))
 
-(defvar core-process-progress-statuses
-  '(run listen open connect))
+(defvar core-process-progress-statuses '(run listen open connect))
+(defvar core-process-poll-timout 0.15)
 
-(defun core-try-start-process (cmd)
+(defun core-try-start-process (cmd &optional show-progress)
   "Invokes passed command in background"
   (interactive "sCommand:")
-  (setq process (get-process cmd))
-  (if process
-      (progn
-        (message "Process already exist: %s" process)
-        (core-update-process-window process 0))
-    (core-run-process cmd)))
+  (let* ((process (get-process cmd)))
+    (if process
+        (progn
+          (message "Process already exist: %s" process)
+          (core-update-process-window process 0))
+      (core-run-process cmd show-progress))))
 
-(defun core-run-process (cmd)
+(defun core-run-process (cmd &optional show-progress)
   (message "Running process '%s'" cmd)
-  (setq buffer-name (concat "*" cmd "*"))
-  (setq buffer (get-buffer-create buffer-name))
-  (setq process (start-process-shell-command cmd buffer cmd))
-  (with-current-buffer buffer (erase-buffer) (shell-mode))
-  (core-handle-progress process))
+  (let* ((buffer-name (concat "*" cmd "*"))
+        (buffer (get-buffer-create buffer-name))
+        (process (start-process-shell-command cmd buffer cmd)))
+    (with-current-buffer buffer (erase-buffer) (shell-mode))
+    (if show-progress
+        (core-handle-progress process)
+      (set-process-sentinel process 'core-handle-result))))
 
 (defun core-handle-progress (process)
-  (setq status (process-status process))
-  (if (member status core-process-progress-statuses)
-      (progn
-        (princ ".")
-        (run-at-time 0.1 nil 'core-handle-progress process))
-    (core-handle-result process)))
+  (let* ((status (process-status process)))
+    (if (member status core-process-progress-statuses)
+        (progn
+          (princ ".")
+          (run-at-time core-process-poll-timout nil 'core-handle-progress process))
+      (core-handle-result process))))
 
-(defun core-handle-result (process)
-  (setq status (process-status process))
-  (setq exit-code (process-exit-status process))
-  (core-refresh-process-buffer process)
-  (if (= exit-code 0)
-      (core-handle-success process status exit-code)
-    (core-handle-error process exit-code)))
+(defun core-handle-result (process &optional event)
+  (let* ((status (process-status process))
+         (exit-code (process-exit-status process)))
+    (core-refresh-process-buffer process)
+    (if (= exit-code 0)
+        (core-handle-success process status exit-code)
+      (core-handle-error process exit-code))))
 
 (defun core-handle-success (process status exit-code)
   (message "Process: %s\nStatus: %s\nCode: %s" process status exit-code)
-  (setq buffer-window (core-get-process-window process))
-  (if buffer-window
-      (delete-window buffer-window)))
+  (let* ((buffer-window (core-get-process-window process)))
+    (if buffer-window
+        (delete-window buffer-window))))
 
 (defun core-handle-error (process exit-code)
   (message "%s ERROR, CODE: %s" process exit-code)
-  (setq buffer (process-buffer process))
-  (setq buffer-window (get-buffer-window buffer))
-  (if (not buffer-window)
-      (progn
-        (setq buffer-window (split-window-vertically))
-        (set-window-buffer buffer-window buffer)))
-  (core-refresh-process-window buffer-window 4))
+  (let* ((buffer (process-buffer process))
+         (buffer-window (get-buffer-window buffer)))
+    (if (not buffer-window)
+        (progn
+          (setq buffer-window (split-window-vertically))
+          (set-window-buffer buffer-window buffer)))
+    (core-refresh-process-window buffer-window)))
 
 (defun core-get-process-window (process)
-  (setq buffer (process-buffer process))
-  (get-buffer-window buffer))
+  (let* ((buffer (process-buffer process)))
+    (get-buffer-window buffer)))
 
 (defun core-refresh-process-buffer (process)
   (with-current-buffer (process-buffer process)
     (ansi-color-apply-on-region (point-min) (point-max))))
 
-(defun core-refresh-process-window (window lines-from-bottom)
+(defun core-refresh-process-window (window)
   (with-selected-window window
-    (scroll-up-command
-     (- (core-get-remaining-lines-count) lines-from-bottom))))
+    (scroll-up-command (core-get-remaining-lines-count))))
 
 (defun core-get-remaining-lines-count ()
     (count-lines (point) (buffer-end 1)))

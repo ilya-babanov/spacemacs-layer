28bba169901f2a497ef75b80b677eedc8d6aaddc
Ilya Babanov <ilya.babanov@gmail.com>
2017-03-25 11:14:30 +0100

    Various misc changes

--- core-misc.el
+++ core-misc.el
@@ -1,11 +1,13 @@
 (defun core-init-misc ()
-  (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+  (setq mouse-wheel-scroll-amount '(2 ((shift) . 2)))
   (setq mouse-wheel-progressive-speed nil)
 
   (setq powerline-default-separator nil)
 
   (spacemacs/toggle-mode-line-minor-modes-off)
-  (spacemacs/toggle-visual-line-navigation-on)
+
+  (add-hook 'comint-mode-hook 'spacemacs/toggle-truncate-lines-on)
+  (add-hook 'prog-mode-hook 'spacemacs/toggle-truncate-lines-on)
 
   (add-hook 'prog-mode-hook (lambda () (core-set-scroll-margin 8)))
   (add-hook 'text-mode-hook (lambda () (core-set-scroll-margin 8)))
@@ -20,10 +22,9 @@
   (add-hook 'comint-mode-hook 'ansi-color-for-comint-mode-on)
   (add-hook 'term-mode-hook 'ansi-color-for-comint-mode-on)
 
-  (setq helm-buffer-max-length 50)
-  (setq eshell-ls-initial-args "-lah")
+  (add-hook 'css-mode-hook 'rainbow-mode)
 
-  (add-hook 'prog-mode-hook (lambda () (visual-line-mode 1)))
+  (setq helm-buffer-max-length 50)
 
   (message "core-misc initialized"))
 

f9b094ce870d8bb6359a85a342ddfd28d24974f4
Ilya Babanov <ilya.babanov@gmail.com>
2017-06-19 23:55:09 +0200

    Update org templates

--- core-org.el
+++ core-org.el
@@ -1,7 +1,8 @@
 (defun core-init-org ()
-  (setq org-agenda-files '("~/organizer.org"))
+  (setq my-org-file "~/org/organizer.org")
+  (setq org-agenda-files `(,my-org-file))
   (setq org-startup-indented t)
-  (setq org-tags-column -140)
+  (setq org-tags-column -100)
   (setq org-startup-with-inline-images t)
 
   (org-babel-do-load-languages
@@ -13,11 +14,14 @@
      (perl . t)))
 
   (setq org-capture-templates
-        '(("t" "Tasks" entry
-           (file "~/organizer.org")
-           "* TODO %?\n%i\n%a\n%T\n")
-          ("n" "Notes" entry
-           (file "~/organizer.org")
+        `(("n" "Note" entry
+           (file+headline ,my-org-file "Notes")
+           "* %?\n\t\n%i\n\n")
+          ("b" "Book" entry
+           (file+headline ,my-org-file "Books")
+           "* %i%?\nAuthor: \n")
+          ("B" "Booking" entry
+           (file+headline ,my-org-file "Booking")
            "* %?\n%i\n")))
 
   (message "core-org initialized"))

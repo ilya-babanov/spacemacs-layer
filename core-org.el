826d01488b145a7a9b6b83fb214e53e6843ed510
Ilya Babanov <ilya.babanov@gmail.com>
2017-06-19 23:03:52 +0200

    Tune org config

--- core-org.el
+++ core-org.el
@@ -2,20 +2,22 @@
   (setq org-agenda-files '("~/organizer.org"))
   (setq org-startup-indented t)
   (setq org-tags-column -140)
+  (setq org-startup-with-inline-images t)
 
   (org-babel-do-load-languages
    'org-babel-load-languages
    '((js . t)
      (python . t)
      (css . t)
-     (shell . t)))
+     (shell . t)
+     (perl . t)))
 
   (setq org-capture-templates
         '(("t" "Tasks" entry
-           (file+headline "~/organizer.org" "Tasks")
+           (file "~/organizer.org")
            "* TODO %?\n%i\n%a\n%T\n")
           ("n" "Notes" entry
-           (file+headline "~/organizer.org" "Notes")
+           (file "~/organizer.org")
            "* %?\n%i\n")))
 
   (message "core-org initialized"))
